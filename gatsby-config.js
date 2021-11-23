b50f776fb69b09894b3e6e2fdd6632902d66bb35
Sprinterson <charles.raviaud@protonmail.com>
2021-11-23 11:23:16 +0100

    feat: create first pages and edit configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,11 @@ module.exports = {
       resolve: "smooth-doc",
       options: {
         name: "GLMAW Doc",
+        navItems: [{ title: 'Docs', url: '/docs/' }, { title: 'Getting Started', url: '/docs/getting-started/' }],
+        sections: ['Guides', 'Reference'],
         description: "Documentation for Groupe Le Monde Ads Wrapper",
         siteUrl: "https://example.com",
+        githubRepositoryURL: 'https://github.com/lemonde/glm-ads-wrapper/',
       },
     },
   ],
