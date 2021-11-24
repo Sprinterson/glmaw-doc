6e11cd1ba06e1e3d8d8ed34679e66962ab4d08a0
Sprinterson <charles.raviaud@protonmail.com>
2021-11-24 11:26:09 +0100

    chore: remove unfinished pages & fix typos

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,8 @@ module.exports = {
       resolve: "smooth-doc",
       options: {
         name: "GLMAW Doc",
-        navItems: [{ title: 'Guide', url: '/docs/' }, { title: 'Plus', url: '/docs/plugins/' }],
-        sections: ['Guide', 'Plus'],
+        navItems: [{ title: 'Guide', url: '/docs/' }],
+        sections: ['Guide'  ],
         description: "Documentation pour Groupe Le Monde Ads Wrapper",
         siteUrl: "https://glmaw-doc.netlify.app/",
         githubRepositoryURL: 'https://github.com/lemonde/glm-ads-wrapper/',
